1ce92d331c9b3a7eddcfd2eec20c63facef7a93e
alicepetzinger <alice.petzinger@gmail.com>
2021-11-03 11:58:10 +0100

    🚀 add welcome set-up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>bergfest music 🦕 </main>;
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>Friday Night Fever at Neue Fische</h1>
+      <h2>Jukebox Sign-In</h2>
+      <form className={styles.form}>
+        <input type="text" placeholder="Hi, call me..." />
+        <input type="text" placeholder="...and my lastname" />
+        <input type="submit" />
+      </form>
+    </main>
+  );
 }
 
 export default App;
